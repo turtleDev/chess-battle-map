4c9c6ac23ed101b6febfe256df8a3240dd21bd68
turtleDev <saravjeetamansingh@gmail.com>
2022-07-10 15:53:15 +0530

    add basic smoke test

--- src/engine.test.js
+++ src/engine.test.js
@@ -1,5 +1,5 @@
 const Engine = require('./engine').default;
 
 test('should throw an exception if gamedata is empty', () => {
-    expect(() => new Engine('')).toThrow();
+    expect(() => new Engine('')).toThrow(new Error('invalid pgn'));
 })
