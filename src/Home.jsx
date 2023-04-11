038c214465c608d4505d6fd51f1c9c1b1932afb4
turtleDev <saravjeetamansingh@gmail.com>
2023-04-11 16:39:57 +0530

    description: update

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ export default function Home() {
         <div className="home">
             <h1>Chess Battle Map</h1>
             <p>Visualise square control from any online chess game.</p>
+            <p>You can also just paste in your PGN.</p>
             <button>demo</button>
         </div>
     )
