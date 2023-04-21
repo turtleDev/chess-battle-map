1fd6c9a3885e057bc12aa34fa8b8b98baf95d007
turtleDev <saravjeetamansingh@gmail.com>
2023-04-21 14:13:38 +0530

    add home button

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,16 @@
-import { Outlet} from "react-router-dom";
+import { faChessBoard } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Base() {
     return (
         <div className="container px-4 md:mx-auto">
-            <div className="flex items-center justify-center">
-                {/* <Link to="/">
-                    <h1 className="text-4xl py-4">Chess Battle Map</h1>
-                </Link> */}
-                <form className="min-w-[75%] flex justify-center items-center rounded my-8 border-4 border-[var(--primary)]">
+            <div className="flex items-center justify-around">
+                <Link to="/">
+                    {/* check how to use fa icon */}
+                    <FontAwesomeIcon className="mx-8" icon={faChessBoard} size="4x"/>
+                </Link>
+                <form className="flex grow justify-center items-center rounded my-8 border-4 border-[var(--primary)]">
                     <input className="grow h-full p-4 focus-visible:outline-0 text-[#303240]" type="text" placeholder="paste game link or PGN to start ..." />
                     <input className="btn mt-0 text-3xl rounded-none hover:cursor-pointer" type="submit" value="start"></input>
                 </form>
