d64f8ba209cefe016814d6832c4d4b5f09470573
turtleDev <saravjeetamansingh@gmail.com>
2023-04-21 19:35:00 +0530

    ui: make homepage symmetrical on mobile

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,13 +5,12 @@ import { Outlet, Link } from "react-router-dom";
 export default function Base() {
     return (
         <div className="container px-4 md:mx-auto">
-            <div className="flex items-center justify-around">
+            <div className="flex items-center">
                 <Link to="/">
-                    {/* check how to use fa icon */}
-                    <FontAwesomeIcon className="mx-8" icon={faChessBoard} size="4x"/>
+                    <FontAwesomeIcon className="mx-2 md:mx-8" icon={faChessBoard} size="4x"/>
                 </Link>
                 <form className="flex grow justify-center items-center rounded my-8 border-4 border-[var(--primary)]">
-                    <input className="grow h-full p-4 focus-visible:outline-0 text-[#303240]" type="text" placeholder="paste game link or PGN to start ..." />
+                    <input className="grow h-full w-10 p-4 focus-visible:outline-0 text-[#303240]" type="text" placeholder="paste game link or PGN to start ..." />
                     <input className="btn mt-0 text-3xl rounded-none hover:cursor-pointer" type="submit" value="start"></input>
                 </form>
             </div>
