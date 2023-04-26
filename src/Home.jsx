f535a3293aad34047c928020fd348be9bb1a24ca
turtleDev <saravjeetamansingh@gmail.com>
2023-04-26 16:27:52 +0530

    ui: different spacing for icon buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -37,13 +37,13 @@ export default function Home() {
                         <Link to="game?data=W0V2ZW50ICJDYXN1YWwgUmFwaWQgZ2FtZSJdCltTaXRlICJodHRwczovL2xpY2hlc3Mub3JnL1JXeUFRNVFXIl0KW0RhdGUgIjIwMjIuMDQuMDIiXQpbV2hpdGUgImJpdGJhbmdlciJdCltCbGFjayAiQW5vbnltb3VzIl0KW1Jlc3VsdCAiMC0xIl0KW1VUQ0RhdGUgIjIwMjIuMDQuMDIiXQpbVVRDVGltZSAiMTI6Mjg6NTEiXQpbV2hpdGVFbG8gIjk4NyJdCltCbGFja0VsbyAiPyJdCltWYXJpYW50ICJTdGFuZGFyZCJdCltUaW1lQ29udHJvbCAiNjAwKzUiXQpbRUNPICJEMjAiXQpbT3BlbmluZyAiUXVlZW4ncyBHYW1iaXQgQWNjZXB0ZWQiXQpbVGVybWluYXRpb24gIlRpbWUgZm9yZmVpdCJdCltBbm5vdGF0b3IgImxpY2hlc3Mub3JnIl0KCjEuIGQ0IGQ1IDIuIGM0IGR4YzQgeyBEMjAgUXVlZW4ncyBHYW1iaXQgQWNjZXB0ZWQgfSAzLiBOYzMgZjUgNC4gZTMgZzUgNS4gUWg1KyBLZDcgNi4gUXhnNSBOZjYgNy4gUXhmNSsgS2U4IDguIFFiNSsgYzYgOS4gUXhjNCBRYTUgMTAuIE5mMyBCZzQgMTEuIEJkMyBCeGYzIDEyLiBneGYzIE5hNiAxMy4gQmQyIE5iNCAxNC4gUmcxIE5oNSAxNS4gUmc4IFJ4ZzggMTYuIFF4ZzggTnhkMysgMTcuIEtlMiBOeGIyIDE4LiBReGg3IFJkOCAxOS4gUWc2KyBLZDcgMjAuIE5kNSBlNiAyMS4gQnhhNSBleGQ1IDIyLiBReGg1IEJhMyAyMy4gQnhkOCBLeGQ4IDI0LiBRZjUgS2M3IDI1LiBoNCBCZTcgMjYuIGg1IEJoNCAyNy4gUWY0KyBLYjYgMjguIFF4aDQgYzUgeyBCbGFjayB3aW5zIG9uIHRpbWUuIH0gMC0x">
                             {/* maybe get extract this to a separate btn-lg class? */}
                             <button className='btn md:px-8 md:py-5 my-6'>
-                                <FontAwesomeIcon className="mr-4" icon={faChessKnight} />
+                                <FontAwesomeIcon className="mr-2 md:mr-4" icon={faChessKnight} />
                                 demo
                             </button>
                         </Link>
                         <button className='btn-inverted md:px-8 md:py-5 my-6'>
                             <a href="https://github.com/turtleDev/chess-battle-map" target="_blank" rel="noreferrer">
-                                <FontAwesomeIcon className="mr-4" icon={faGithub}/>
+                                <FontAwesomeIcon className="mr-2 md:mr-4" icon={faGithub}/>
                                 source
                             </a>
                         </button>
